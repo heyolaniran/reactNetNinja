c5124fab08245d83cbb5a95cd7ad18b8ce368b69
heyolaniran <olaniranlaurent@gmail.com>
2023-03-02 16:05:22 +0100

    feat: onClick event notion

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 export default function Home ()
 {
     const likes = 80 
+    const handleClick = (likes) => {
+        likes = likes + 1 
+        console.log(" now React i liked " + likes + " M times") ; 
+    }
     return (
         <div className="mt-4">
             <h1 className="text-center text-2xl">
@@ -9,6 +13,15 @@ export default function Home ()
             <p>
                 React Is an Javacript library liked more than <span className='font-semibold'>{likes} </span>M times 
             </p>
+
+
+            <div className="justify-center flex mt-4">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded-full font-semibold"
+                    onClick={ () => handleClick(likes)}
+                >
+                    Click me 
+                </button>
+            </div>
         </div>
     )
 }
