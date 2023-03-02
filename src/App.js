296c43dc98d256125173ba5116f09abbfbeb64f6
heyolaniran <olaniranlaurent@gmail.com>
2023-03-02 15:22:01 +0100

    feat: template & components notion

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-semibold text-black">
+      
+        <h1 className="text-xl font-normal text-black">
         Hello world!
       </h1>
-      </header>
-
       
     </div>
   );
