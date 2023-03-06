d6b5206c31c6dc7ac2c152717b56dc3c02ba3532
heyolaniran <olaniranlaurent@gmail.com>
2023-03-06 13:23:22 +0100

    feat: routes initializations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import NavBar from './NavBar';
 import Home from './Home';
-
+import {BrowserRouter as Router , Routes, Route } from 'react-router-dom'
 function App() {
 
  
   return (
-    
-    <div className="App">
+    <Router>
+      <div className="App">
 
-      <NavBar/>
-      
-        <Home />
-      
-    </div>
+        <NavBar/>
+        
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+        
+       
+        
+      </div>
+    </Router>
+    
   );
 }
 
