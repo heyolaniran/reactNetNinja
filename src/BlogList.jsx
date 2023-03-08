bb6bf9658ed67cc690119a93b2c1ca8f9d8379a7
heyolaniran <olaniranlaurent@gmail.com>
2023-03-08 15:13:55 +0100

    feat: reactNetNinja mastering crash course done

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -5,22 +5,39 @@ export default function BlogList ({blogs, title}) {
     return (
         <div className="blogList mt-4">
 
+
             <div>
                 <h2 className="text-center text-2xl"> {title }</h2>
-                {
-                    blogs.map((blog) => (
-                        <div className="hover:shadow-xs shadow-lg mb-4 bg-white rounded-lg px-6 py-2" key={blog.id}>
-                            <Link to={`/blog/${blog.id}`}>
-                                <h1 className="text-2xl font-semibold text-blue-600">{blog.title}</h1>
-                            </Link>
-                            
-                            <span className="text-md"> By {blog.author}</span>
-                            <div className="mt-4 justify-end flex">
+                <div class="grid grid-cols-1 md:grid-cols-4 py-4 ">
+                   {
+                        blogs.map((blog) => (
+                            <article className=" mt-2 mr-4 rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm" key={blog.id}>
+                                <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
+                                    <time datetime="2022-10-10" className="block text-xs text-gray-500">
+                                    by {blog.author}
+                                    </time>
 
-                            </div>
-                        </div>
-                    ))
-                }
+                                    <Link to={`/blog/${blog.id}`}>
+                                        <h3 className="mt-0.5 text-lg font-medium text-gray-900">
+                                            {blog.title}
+                                        </h3>
+                                    </Link>
+
+                                    <div className="mt-4 flex flex-wrap gap-1">
+                                    
+                                        <span
+                                            className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600"
+                                        >
+                                            Article
+                                        </span>
+                                    </div>
+                                </div>
+                            </article>
+                            
+                        ))
+                    }
+                </div>
+               
             </div>
         </div>
     )
